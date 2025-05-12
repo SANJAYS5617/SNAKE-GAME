68d488bde6f5a6be5bd813022f9c1785dda18c0e
SANJAYKUMAR B <144211946+SANJAYS5617@users.noreply.github.com>
2025-05-12 09:51:26 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package snakeGame;
+
+public class Main {
+    public static void main(String[] args) {
+        Snake snake = new Snake(6,6);
+        snake.snakeMove(0,0); 
+
+       
+    }
+}
+
